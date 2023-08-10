fdf7a26b4918a21ec2575f24f76bb31867a8fe71
Luke <lucasleonyuan@gmail.com>
2023-08-10 23:58:00 +0000

    db typo and updater

--- db_handler.py
+++ db_handler.py
@@ -4,7 +4,7 @@ from pymongo.database import Database
 
 class DatabaseHandle:
     def __init__(self, mongo_host:str = "localhost", mongo_port:int = 27017):
-        self.client: MongoClient = MongoClient(host= mongo_host, port = mongo_port)
+        self.client: MongoClient = MongoClient(host = mongo_host, port = mongo_port)
         self.db: Database = None
 
     def set_db(self, database:str):
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     
     #for db updates
     handle = DatabaseHandle()
-    handle.set_db("test")
+    handle.set_db("live")
 
     handle.disconnect()
